ea0c037f9bb7ff6d05b69d0cc2ffd27ef8ad7ab0
AlexJara920318 <87007913+AlexJara920318@users.noreply.github.com>
2021-09-15 12:26:19 +0200

    Solved some points

--- Patient Sensors - Simulation/Curves.py
+++ Patient Sensors - Simulation/Curves.py
@@ -71,12 +71,11 @@ class Simulation():
             self.sf = np.append(self.sf,s6)
 
         if danger == "y":
-            HAm1 = np.random.randint(15,40,1)
-            HAm2 = np.random.randint(40,72,2)
+
             HAv = np.random.randint(200,220,3)
-            self.sf[HAm1] = HAv[0]
-            for j in range(2):
-                self.sf[HAm2[j-1]] = HAv[j]
+            self.sf[12] = HAv[0]
+            self.sf[22] = HAv[1]
+            self.sf[38] = HAv[2]
 
         return self.sf
     
@@ -115,12 +114,11 @@ class Simulation():
             self.sf = np.append(self.sf,s6)
 
         if danger == "y":
-            HAm1 = np.random.randint(15,40,1)
-            HAm2 = np.random.randint(40,72,2)
+
             HAv = np.random.randint(200,220,3)
-            self.sf[HAm1] = HAv[0]
-            for j in range(2):
-                self.sf[HAm2[j-1]] = HAv[j]    
+            self.sf[16] = HAv[0]
+            self.sf[29] = HAv[0]
+            self.sf[42] = HAv[0]
 
         return self.sf
 
@@ -149,12 +147,11 @@ class Simulation():
             self.sf = np.append(self.sf,s6)
 
         if fever == "y":
-            HAm1 = np.random.randint(24,32,1)
-            HAm2 = np.random.randint(51,60,2)
+
             HAv = np.random.uniform(37.8,39.2,3)
-            self.sf[HAm1] = HAv[0]
-            for j in range(2):
-                self.sf[HAm2[j-1]] = HAv[j]
+            self.sf[10] = HAv[0]
+            self.sf[24] = HAv[1]
+            self.sf[33] = HAv[2]
 
         return self.sf
     
@@ -221,121 +218,6 @@ class Simulation():
             s6 = np.append(s5,s6)
 
             self.sf = np.append(self.sf,s6)
-
-        return self.sf
-
-    def Norm_OS(self):
-
-        self.sf = []
-
-        t1 = np.arange(0.0, 1.0, 1/24)
-        s1 = np.sin((3 * 2 * np.pi * t1)-3/2)
-
-        for i in range(3):
-
-            rand_func1 = np.random.uniform(self.a-1, self.a+1, 4)
-            s4 = rand_func1
-
-            rand_func2 = np.random.uniform(self.a+1, self.b-1, 3)
-            s5 = s1[0:3] + rand_func2
-
-            rand_func3 = np.random.uniform(self.b-1, self.b, 10)
-            s6 = rand_func3
-
-            rand_func4 = np.random.uniform(self.a+1, self.b-1, 3)
-            s7 = s1[21:24] + rand_func2
-       
-            rand_func5 = np.random.uniform(self.a-1, self.a+1, 4)
-            s8 = rand_func5
-       
-            s5 = np.append(s4,s5)
-            s6 = np.append(s5,s6)
-            s7 = np.append(s6,s7)
-            s8 = np.append(s7,s8)
-            self.sf = np.append(self.sf,s8)
-
-        return self.sf
-
-    def Low_OS(self):
-        
-        self.sf = []
-
-        t1 = np.arange(0.0, 1.0, 1/24)
-        s1 = np.sin((3 * 2 * np.pi * t1)-3/2)
-
-        for i in range(3):
-
-            rand_func1 = np.random.uniform(self.a-1, self.a+1, 4)
-            s4 = rand_func1
-
-            rand_func2 = np.random.uniform(self.a+1, self.b-1, 3)
-            s5 = s1[0:3] + rand_func2
-
-            rand_func3 = np.random.uniform(self.b-1, self.b, 10)
-            s6 = rand_func3
-
-            rand_func4 = np.random.uniform(self.a+1, self.b-1, 3)
-            s7 = s1[21:24] + rand_func2
-       
-            rand_func5 = np.random.uniform(self.a-1, self.a+1, 4)
-            s8 = rand_func5
-       
-            s5 = np.append(s4,s5)
-            s6 = np.append(s5,s6)
-            s7 = np.append(s6,s7)
-            s8 = np.append(s7,s8)
-            self.sf = np.append(self.sf,s8)   
-
-        HAm1 = np.random.randint(1,24,1)
-        HAm2 = np.random.randint(25,48,2)
-        HAm3 = np.random.randint(49,72,4)
-        HAv = np.random.uniform(87.0,93.0,7)
-        
-
-        for j in range(7):
-            if j == 0:
-                self.sf[HAm1] = HAv[j]  
-            elif j>0 and j<3:
-                self.sf[HAm2[j-1]] = HAv[j]
-            elif j>2:
-                self.sf[HAm3[j-3]] = HAv[j]
-        
-        return self.sf
-
-    def Dang_OS(self):
-
-        self.sf = []
-
-        t1 = np.arange(0.0, 1.0, 1/24)
-        s1 = np.sin((3 * 2 * np.pi * t1)-3/2)
-
-        for i in range(3):
-            if i == 1:
-                self.a = 85.0
-                self.b = 92.0
-            elif i == 2:
-                self.a = 81.0
-                self.b = 89.0
-            rand_func1 = np.random.uniform(self.a-1, self.a+1, 4)
-            s4 = rand_func1
-
-            rand_func2 = np.random.uniform(self.a+1, self.b-1, 3)
-            s5 = s1[0:3] + rand_func2
-
-            rand_func3 = np.random.uniform(self.b-1, self.b, 10)
-            s6 = rand_func3
-
-            rand_func4 = np.random.uniform(self.a+1, self.b-1, 3)
-            s7 = s1[21:24] + rand_func2
-       
-            rand_func5 = np.random.uniform(self.a-1, self.a+1, 4)
-            s8 = rand_func5
-       
-            s5 = np.append(s4,s5)
-            s6 = np.append(s5,s6)
-            s7 = np.append(s6,s7)
-            s8 = np.append(s7,s8)
-            self.sf = np.append(self.sf,s8)
 
         return self.sf
 
